506cd9d6235015583ffe36de1b7c8934dae449c1
Antonio Augusto Abello (MSc | hirata) <abello@.ime.usp.br>
2018-11-04 03:07:28 -0200

    imports and bugs

--- test_sisr.py
+++ test_sisr.py
@@ -7,23 +7,30 @@ from skimage.util import img_as_ubyte
 from skimage.measure import compare_ssim as ssim
 from skimage.measure import compare_psnr as psnr
 
+from skimage.transform import rescale as rescale
+from scipy.misc import imresize as imresize
+from skimage.util import invert
+
+import itertools
+
 def generate_baseline(img):
     smol = rescale(img, 0.5, anti_aliasing=True, multichannel=False, mode='reflect')
-    baseline = imresize(smol, (94, 104), interp="bicubic") #baseline do artigo é interpolação bicubica
+    baseline = imresize(smol, img.shape, interp="bicubic") #baseline do artigo é interpolação bicubica
     return baseline
 
-def generate_prediction(baseline, model, black=False):
+def generate_prediction(img, model, black=False):
     if (black):
-        baseline = invert(baseline)
+        baseline = invert(img)
 
-    output = np.zeros(baseline.shape)
-    overlap = np.zeros(baseline.shape)
+    smol = rescale(img, 0.5, anti_aliasing=True, multichannel=False, mode='reflect')
+    output = np.zeros(img.shape)
+    overlap = np.zeros(img.shape)
 
     #Previmos cada patch de 20x20 na imagem, somamos e tiramos a média
     for y, x in itertools.product(range(0, smol.shape[0] - 19), range(0, smol.shape[1] - 19)):
         predpatch = smol[y:y + 20, x: x + 20]
         predpatch = imresize(predpatch, (40, 40), interp='bicubic')
-        predpatch = black_net.predict(predpatch.reshape((1,) + predpatch.shape + (1,)))[0]
+        predpatch = model.predict(predpatch.reshape((1,) + predpatch.shape + (1,)))[0]
         predpatch = predpatch.reshape(predpatch.shape[0:2])
         output[y * 2: y * 2 + 40, x * 2: x * 2 + 40] += predpatch
         overlap[y * 2: y * 2 + 40, x * 2: x * 2 + 40] += 1
@@ -36,51 +43,60 @@ def generate_prediction(baseline, model, black=False):
     output_int = img_as_ubyte(output_int)
 
     if (black):
-        output = invert(output)
+        output_int = invert(output_int)
 
-    return output
+    return output_int
 
 def mse(y, x):
     return np.sum((y - x) ** 2) / np.prod(y.shape)
     
-#Carrega Dados
 dataset = np.load("test_dataset.npy")
 model = load_model("CNNSISR_black")
 
-mse_base, psnr_base, ssim_base = (0, 0, 0)
-mse, psnr, ssim = (0, 0, 0)
+#Carrega Dados
+if __name__ == "__main__":
 
-for test in dataset:
-    baseline = generate_baseline(test)
-    preds = generate_prediction(baseline, model, black=True)
+    mse_base, psnr_base, ssim_base = (0, 0, 0)
+    mse_pred, psnr_pred, ssim_pred = (0, 0, 0)
 
-    mse_base += mse(baseline, test)
-    mse += mse(preds, test)
+    for test in dataset:
     
-    psnr_base += psnr(baseline, test)
-    psnr += psnr(preds, test)
+        if (test.shape[0] % 2 == 1):
+            test = test[0:-1, :]
+
+        if (test.shape[1] % 2 == 1):
+            test = test[:, 0:-1]
     
-    ssim_base += ssim(baseline, test)
-    ssim += ssim(preds, test)
+        baseline = generate_baseline(test)
+        preds = generate_prediction(test, model, black=True)
 
-mse_base /= len(dataset)
-mse /= len(dataset)
+        mse_base += mse(baseline, test)
+        mse_pred += mse(preds, test)
+    
+        psnr_base += psnr(baseline, test)
+        psnr_pred += psnr(preds, test)
+    
+        ssim_base += ssim(baseline, test)
+        ssim_pred += ssim(preds, test)
 
-psnr_base /= len(dataset)
-psnr /= len(dataset)
+    mse_base /= len(dataset)
+    mse /= len(dataset)
 
-ssim_base /= len(dataset)
-ssim /= len(dataset)
+    psnr_base /= len(dataset)
+    psnr /= len(dataset)
 
-print("Resultados")
-print("")
-print("Baseline")
-print("MSE: %f " % mse_base)
-print("PSNR: %f " % psnr_base)
-print("SSIM: %f " % ssim_base)
-print("")
-print("Modelo")
-print("MSE: %f " % mse)
-print("PSNR: %f " % psnr)
-print("SSIM: %f " % ssim)
+    ssim_base /= len(dataset)
+    ssim /= len(dataset)
+
+    print("Resultados")
+    print("")
+    print("Baseline")
+    print("MSE: %f " % mse_base)
+    print("PSNR: %f " % psnr_base)
+    print("SSIM: %f " % ssim_base)
+    print("")
+    print("Modelo")
+    print("MSE: %f " % mse_pred)
+    print("PSNR: %f " % psnr_pred)
+    print("SSIM: %f " % ssim_pred)
 
